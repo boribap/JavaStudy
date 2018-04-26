8f95ac5ae5f10b3ac1b43d33a56a1c4ec91eea1a
bowon <bsww201@naver.com>
2018-04-27 01:24:48 +0900

    java st

---
+++ code/base01/ch6/src/src/InfRecul.java
@@ -0,0 +1,13 @@
+public class InfRecul {
+    public static void main(String argc[]){
+        showHi(3);
+    }
+
+    public static void showHi(int cnt){
+        System.out.println("Hi! ");
+        showHi(cnt--);
+        if(cnt == 1){
+            return;
+        }
+    }
+}
