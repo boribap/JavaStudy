ecea6a62f545c36ed50b12c53bcea3c3045fe7ed
bowon <bsww201@naver.com>
2018-06-10 01:48:48 +0900

    java_rectangle_example_done

--- code/ch9/src/RectangleTest.java
+++ code/ch9/src/RectangleTest.java
@@ -1,7 +1,38 @@
 class Rectangle
 {
-    int ulx, uly; //좌 상단 x,y 좌표
-    int lrx, lry; //우 하단 x,y 좌표
+    private int ulx, uly; //좌 상단 x,y 좌표
+    private int lrx, lry; //우 하단 x,y 좌표
+
+    private boolean isProperRange(int pos)
+    {
+        if(pos >= 0 && pos <= 100) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public void setRectanglePoint(int lx, int ly, int rx, int ry)
+    {
+        if(rx <= lx || ry <= ly)
+        {
+            System.out.println("잘못된 좌표값");
+            return;
+        }
+        if(!isProperRange(lx) || !isProperRange(ly))
+        {
+            System.out.println("좌상단의 좌표 범위가 잘못되었습니다.");
+            return;
+        }
+        if (!isProperRange(rx) || !isProperRange(ry))
+        {
+            System.out.println("우상단의 좌표 범위가 잘못되었습니다.");
+            return;
+        }
+
+        ulx = lx; uly = ly;
+        lrx = rx; lry = ry;
+    }
 
     public void showArea()
     {
@@ -16,8 +47,10 @@ public class RectangleTest
     public static void main(String[] args)
     {
         Rectangle rec = new Rectangle();
-        rec.ulx = 22; rec.uly = 22;
-        rec.lrx = 10; rec.lry = 10;
+        rec.setRectanglePoint(-3,-1,2,7);
+        rec.showArea();
+
+        rec.setRectanglePoint(2,2,8,8);
         rec.showArea();
     }
 }
