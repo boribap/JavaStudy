1e9e317191fec29d42bc70fa88a123a2713fac38
bowon <bsww201@naver.com>
2018-06-15 01:00:33 +0900

    java_ch13_04

--- code/ch13/src/EnhancedForInst.java
+++ code/ch13/src/EnhancedForInst.java
@@ -14,6 +14,9 @@ public class EnhancedForInst
                 new Number(8)
         };
 
+        // 배열에 저장되어 있는 인스턴스 멤버 num의 값을 1씩 증가시키고 있음.
+        // 이는 배열에 저장되어 있는 참조 값을 통해서, 인스턴스의 값을 변경시킨것.
+        // 즉 배열에 저장된 참조값을 변경시킨것이 아니기 때문에, 인스턴스의 값은 실제 변경이 일어남.
         for (Number e : arr)
         {
             e.num++;
@@ -25,6 +28,8 @@ public class EnhancedForInst
         }
         System.out.println("");
 
+        // 배열의 참조를 위해 선언된 변수 e의 값을 변경시키고 있음
+        // for-each 내에서 변경된 e값이므로 그 안에서만 유효. 그래서 이 문장을 통해 이뤄진 연산의 결과는 실제 배열에 반영되지 않는다. 
         for(Number e : arr)
         {
             e = new Number(5);
